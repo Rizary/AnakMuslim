d658535f6150bfed817d42a5d0bc384403b2c1b6
Rizary <andika.riyan@gmail.com>
2016-05-05 12:32:58 +0700

    Merger Wudhu

---
+++ app/src/main/java/id/aplikasimuslim/anakmuslim/Wudhu.java
@@ -0,0 +1,37 @@
+package id.aplikasimuslim.anakmuslim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class Wudhu extends AppCompatActivity implements View.OnClickListener{
+
+    ViewFlipper viewFlipper;
+    Button next;
+    Button previous;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wudhu);
+
+        viewFlipper = (ViewFlipper)findViewById(R.id.viewFlipper);
+        next = (Button) findViewById(R.id.next);
+        previous = (Button) findViewById(R.id.previous);
+
+        next.setOnClickListener(this);
+        previous.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        if (v == next){
+            viewFlipper.showNext();
+
+        }
+        else if (v==previous){
+            viewFlipper.showPrevious();
+        }
+    }
+}
